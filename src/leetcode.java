4fd49944e19875291b8b188cdb92635ea58bd255
Song Zhao <lanmovii@gmail.com>
2015-08-09 20:56:26 -0500

    reverse lnikedlist

--- src/leetcode.java
+++ src/leetcode.java
@@ -149,7 +149,7 @@ public class leetcode {
 			else
 				digits[i] = 0;
 		}
-		  z
+		  
 		//if we completed the loop and didnt return, this means we have an '1' carry out and all other bits are 0;
 		int[] res = new int[digits.length + 1];
 		res[0] = 1;
@@ -1089,6 +1089,7 @@ public class leetcode {
 					 
 			 }
 		 }
+		 return res;
 	 }
 	 
 	 //combination
@@ -1440,7 +1441,27 @@ public class leetcode {
 		 //otherwise, way += numDecondings(s.substring(2));
 		 // use cache to reduce time.
 	 
-		 
+		 //reverse linkedlist
+		    public ListNode reverseBetween(ListNode head, int m, int n) {
+		        ListNode dummy=new ListNode(0);
+		        dummy.next=head;
+		        ListNode pre=dummy; //pre is the node before orignal M
+		        ListNode M=head;    //M is after pre
+
+		        for(int i=1;i<m;i++){ //Move pre and M to orignal place
+		            pre=pre.next;
+		            M=M.next;
+		        }
+
+		        for(int i=0;i<n-m;i++){ 
+		            ListNode current=M.next; //Both pre and M are all fixed, only current is assigned every time to M.next. M is pushed back everytime
+		            M.next=current.next;     //Move current to the position after pre
+		            current.next=pre.next;
+		            pre.next=current;
+		        }
+
+		        return dummy.next;
+		}
 	 
 	
 	
