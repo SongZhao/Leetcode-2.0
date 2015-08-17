877f84e9392528b1a9a5ce95eb864a30fffe3514
Song Zhao <lanmovii@gmail.com>
2015-08-17 17:14:03 -0500

    8/17

--- src/leetcode.java
+++ src/leetcode.java
@@ -1638,9 +1638,50 @@ public class leetcode {
 	           		}
 	           		return result;
 	           }
+	    
+	         //interleaving String   rec **
+	           private static Set<Integer> visited; // The combination of i1, i2 has been visited and return false
+	           public static boolean isInterleave(String s1, String s2, String s3) {
+	               if(s3.length() != s1.length() + s2.length())
+	                   return false;
+	               visited = new HashSet<Integer>();
+	               return isInterleave(s1, 0, s2, 0, s3, 0);
+	           }
+	           private static boolean isInterleave(String s1, int i1, String s2, int i2, String s3, int i3)
+	           {   
+	               int hash = i1 * s3.length() + i2;
+	               if(visited.contains(hash))
+	                   return false;
+
+	               if(i1 == s1.length())
+	                   return s2.substring(i2).equals(s3.substring(i3));
+	               if(i2 == s2.length())
+	                   return s1.substring(i1).equals(s3.substring(i3));
+
+	               if(s3.charAt(i3) == s1.charAt(i1) && isInterleave(s1, i1+1, s2, i2, s3, i3+1) ||
+	                  s3.charAt(i3) == s2.charAt(i2) && isInterleave(s1, i1, s2, i2+1, s3, i3+1))
+	                   return true;
+
+	               visited.add(hash);
+	               return false;
+	           }
 	
-	
-	
+	         //Validate Binary Search tree
+	           public static boolean isValidBST(TreeNode root)
+	           {
+	        	   return ValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+	           }
+	           public static boolean ValidBST(TreeNode root, long min, long max)
+	           {
+	        	   if(root == null)
+	        		   return true;
+	       
+	        	   if((long)root.val > min && (long)root.val < max)
+	        	   {
+	        		   return (ValidBST(root.right, root.val, max)&&ValidBST(root.left, min, root.val));
+	        	   }
+	        	   return false;
+	           }
 	
 	
 	
