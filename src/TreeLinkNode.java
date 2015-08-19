dd2fc61973e1ac738456ebb573fa2ab3b0e8cd01
Song Zhao <lanmovii@gmail.com>
2015-08-19 10:51:57 -0500

    8/19

---
+++ src/TreeLinkNode.java
@@ -0,0 +1,11 @@
+
+public class TreeLinkNode {
+	  int val;
+	  TreeLinkNode(int x) {
+         val = x;
+          next = null;
+	  }
+	  TreeLinkNode left;
+      TreeLinkNode right;
+      TreeLinkNode next;
+}
