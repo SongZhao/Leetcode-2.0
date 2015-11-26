9058583123f0e57520dcc2eb951aaa78b5736296
Song Zhao <lanmovii@gmail.com>
2015-11-26 16:34:00 -0600

    tg

--- src/things_to_do_with_hashtable.java
+++ src/things_to_do_with_hashtable.java
@@ -361,5 +361,26 @@ public class things_to_do_with_hashtable {
     return true;
 
     }
+	 
+	/* Collect the set of things we see, encoded as strings. For example:
+
+		 '4' in row 7 is encoded as "(4)7".
+		 '4' in column 7 is encoded as "7(4)".
+		 '4' in the top-right block is encoded as "0(4)2".
+		 Scream false if we ever fail to add something because it was already added (i.e., seen before).*/
+
+	 public boolean isValidSudoku(char[][] board) {
+	     Set seen = new HashSet();
+	     for (int i=0; i<9; ++i) {
+	         for (int j=0; j<9; ++j) {
+	             if (board[i][j] != '.') {
+	                 String b = "(" + board[i][j] + ")";
+	                 if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
+	                     return false;
+	             }
+	         }
+	     }
+	     return true;
+	 }
 	
 }
