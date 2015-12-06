9f123b5e4625cc69173dd654f7939f3c9b70e45a
Song Zhao <lanmovii@gmail.com>
2015-12-05 18:48:05 -0600

    linked list review

--- src/things_to_do_with_linkedlist.java
+++ src/things_to_do_with_linkedlist.java
@@ -39,4 +39,57 @@ public class things_to_do_with_linkedlist {
         }
         return res;
     }
+    
+    
+    
+    
+    
+    
+    
+    //Add two numbers
+    //you are given two linked lists representing two non-negative numbers. 
+    //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    //Output: 7 -> 0 -> 8
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        int carry = 0;
+  		ListNode head = new ListNode(0);
+  		ListNode res = head;
+  		int first;
+  		int second;
+  		while(l1 != null||l2 != null||carry != 0)
+  		{
+  			first = (l1 == null)?0:l1.val;
+  			second = (l2 == null)?0:l2.val;
+  			carry = carry + first + second;
+  			head.next = new ListNode(carry%10);
+  			head = head.next;
+  			carry = carry/10;
+  			l1 = (l1 == null)?null:l1.next;
+  			l2 = (l2 == null)?null:l2.next;
+  			
+  		}
+  		return res.next;
+    }
+    
+    //remove the nth node
+    public ListNode removeNthFromEnd(ListNode head, int n) 
+    {
+        ListNode first = head;
+        ListNode second = head;
+        while(n > 0)
+        {
+            first = first.next;
+            n--;
+        }
+        if(first == null)
+            return second.next;
+        while(first.next != null)
+        {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return head;
+    }
 }
