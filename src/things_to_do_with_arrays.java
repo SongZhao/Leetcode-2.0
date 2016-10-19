889b17f99d3c84f28217588997005aada5de10c8
Song Zhao <lanmovii@gmail.com>
2016-10-18 23:47:38 -0500

    hw

--- src/things_to_do_with_arrays.java
+++ src/things_to_do_with_arrays.java
@@ -112,5 +112,44 @@ public class things_to_do_with_arrays {
 		Arrays.sort(nums);
 		return nums[nums.length/2];
 	}
+	//Majority element II
+	//this time we are trying to find elements that apppear >= n/3
+	//note that >= n/3 means there are at most two elements that can
+	//satisfy this
 
+    public List<Integer> majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return new ArrayList<Integer>();
+        List<Integer> result = new ArrayList<Integer>();
+        int number1 = nums[0], number2 = nums[0], count1 = 0, count2 = 0, len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == number1)
+                count1++;
+            else if (nums[i] == number2)
+                count2++;
+            else if (count1 == 0) {
+                number1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                number2 = nums[i];
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == number1)
+                count1++;
+            else if (nums[i] == number2)
+                count2++;
+        }
+        if (count1 > len / 3)
+            result.add(number1);
+        if (count2 > len / 3)
+            result.add(number2);
+        return result;
+    }
 }
