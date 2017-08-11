d132ec51ade310e8cd9507b99250f1e0c3f02eaf
Song Zhao <lanmovii@gmail.com>
2017-08-10 20:30:56 -0500

    next pum

--- src/things_to_do_with_arrays.java
+++ src/things_to_do_with_arrays.java
@@ -240,4 +240,30 @@ public class things_to_do_with_arrays {
 		}
 	}
 
+	/*Next Permutation
+	Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+	If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+	The replacement must be in-place, do not allocate extra memory.
+	Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+	1,2,3 → 1,3,2
+	3,2,1 → 1,2,3
+	1,1,5 → 1,5,1
+	 */
+	public void nextPermutation(int[] nums) {
+		if(nums.length == 0 || nums.length == 1)
+			return;
+		int i = nums.length - 1;
+		for(; i > 0 && nums[i-1] >= nums[i]; i--);
+		if(i == 0)
+		{
+			Arrays.sort(nums);
+			return;
+		}
+		int j = nums.length - 1;
+		for(; j > 0 && nums[i-1] >= nums[j]; j--);
+		int tmp = nums[j];
+		nums[j] = nums[i-1];
+		nums[i-1] = tmp;
+		Arrays.sort(nums, i, nums.length);
+	}
 }
