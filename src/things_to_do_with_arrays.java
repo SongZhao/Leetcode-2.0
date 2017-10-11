9aeefcc65cc3d25dc5372dd22f76a24f787c6d4f
Song Zhao <lanmovii@gmail.com>
2017-10-10 22:03:34 -0500

    10/10

--- src/things_to_do_with_arrays.java
+++ src/things_to_do_with_arrays.java
@@ -475,4 +475,47 @@ public class things_to_do_with_arrays {
     res.add(newInterval); //if the merge happenend till the end of the loop, we need to add it here.
     return res;
 	}
+
+
+	//Search a 2D Matrix
+	public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0)
+            return false;
+        int start = 0, rows = matrix.length, cols = matrix[0].length;
+        int end = rows*cols - 1;
+        //n * m matrix convert to an array => matrix[x][y] => a[x * m + y]
+		//current row = index/cols, current col = index%cols;
+        while(start <= end) //must be <= for the  case where start = end
+        {
+            int mid = (start + end)/2;
+            if(matrix[mid/cols][mid%cols] == target)
+                return true;
+            if(matrix[mid/cols][mid%cols] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return false;
+    }
+
+    //Sort Colors
+    //tricky three pointer
+    public void sortColors(int[] nums) {
+         int p1 = 0, p2 = nums.length - 1, index = 0;
+         while (index <= p2) 
+            {
+                if (nums[index] == 0) {
+                    nums[index] = nums[p1];
+                    nums[p1] = 0;
+                    p1++;
+                }
+                if (nums[index] == 2) {
+                    nums[index] = nums[p2];
+                    nums[p2] = 2;
+                    p2--;
+                    index--;
+                }
+                index++;
+        }
+    }
 }
