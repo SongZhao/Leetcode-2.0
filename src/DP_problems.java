711c95d12e9c917fdab40c88af445652cfe7ac9d
Song Zhao <lanmovii@gmail.com>
2017-10-23 23:55:02 -0500

    23/11

--- src/DP_problems.java
+++ src/DP_problems.java
@@ -365,5 +365,15 @@ public class DP_problems{
         minDistanceMP[len_s - 1][len_t - 1] = res;
         return res;
     }
+
+
+    //Maximal Rectangle
+    //Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+    //For example, given the following matrix:
+    //1 0 1 0 0
+    //1 0 1 1 1
+    //1 1 1 1 1
+    //1 0 0 1 0
+    //return 6
 }
 
