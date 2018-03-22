6e40d9d2608db97f590e97c65715b57fc35dd498
SongZhao <lanmovii@gmail.com>
2018-03-21 23:38:32 -0500

    max product

--- src/things_to_do_with_arrays.java
+++ src/things_to_do_with_arrays.java
@@ -879,5 +879,17 @@ public class things_to_do_with_arrays {
         return area;
     }
 
-
+    //Maximum Product Subarray
+    //Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){ int tmp = imax; imax = imin; imin = tmp;}
+            imax = Math.max(imax*nums[i], nums[i]);
+            imin = Math.min(imin*nums[i], nums[i]);
+            
+            max = Math.max(max, imax);
+        }
+        return max;
+    }
 }
