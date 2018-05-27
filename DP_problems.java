25aef60ef83739f5a40eae9f5d8694725ec17fb5
Song Zhao <lanmovii@gmail.com>
2018-05-26 20:25:18 -0500

    palindrome

--- DP_problems.java
+++ DP_problems.java
@@ -862,15 +862,33 @@ public class DP_problems{
     }
 
 
+    /*
+    Given a string, your task is to count how many palindromic substrings in this string.
 
-    /*You are given n pairs of numbers. In every pair, the first number is always 
-    smaller than the second number. Now, we define a pair (c, d) can follow another 
-    pair (a, b) if and only if b < c. Chain of pairs can be formed in this fashion.
-    Given a set of pairs, find the length longest chain which can be formed. You needn't 
-    use up all the given pairs. You can select pairs in any order.
-    */
+    The substrings with different start indexes or end indexes are counted as different 
+    substrings even they consist of same characters.
+   */
+
+    int count =1;
+    public int countSubstrings(String s) {
+        if(s.length()==0) 
+            return 0;
+        for(int i=0; i<s.length()-1; i++){
+            checkPalindrome(s,i,i);     //To check the palindrome of odd length palindromic sub-string
+            checkPalindrome(s,i,i+1);   //To check the palindrome of even length palindromic sub-string
+        }
+        return count;
+    }    
+
+    private void checkPalindrome(String s, int i, int j) {
+        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){    //Check for the palindrome string 
+            count++;    //Increment the count if palindromin substring found
+            i--;    //To trace string in left direction
+            j++;    //To trace string in right direction
+        }
+    }
 
 
-    
+
 }
 
