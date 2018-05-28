efe647c737fe434e370330770a6f7e19b23ebeb8
Song Zhao <lanmovii@gmail.com>
2018-05-27 22:20:23 -0500

    dp

--- DP_problems.java
+++ DP_problems.java
@@ -888,6 +888,35 @@ public class DP_problems{
         }
     }
 
+    /*
+    Given two strings s1, s2, find the lowest ASCII sum of deleted characters to make two strings equal.
+
+    Example 1:
+    Input: s1 = "sea", s2 = "eat"
+    Output: 231
+    Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum.
+    Deleting "t" from "eat" adds 116 to the sum.
+    At the end, both strings are equal, and 115 + 116 = 231 is the minimum sum possible to achieve this.
+    */
+    /**
+    * dp[i][j] = a[i] == b[j] ? dp[i + 1][j + 1] :
+    *            min(a[i] + dp[i + 1][j],  // delete a[i] + minimumDeleteSum(a.substr(i+1), b.substr(j))
+    *                b[j] + dp[i][j + 1])  // delete b[j] + minimumDeleteSum(a.substr(i), b.substr(j+1))
+    */
+    
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length(), n = s2.length(), MAX = Integer.MAX_VALUE;
+        char[] a = s1.toCharArray(), b = s2.toCharArray();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = m; i >= 0; i--) {
+            for (int j = n; j >= 0; j--) {
+                if (i < m || j < n)
+                    dp[i][j] = i < m && j < n && a[i] == b[j] ?
+                        dp[i + 1][j + 1] : Math.min((i < m ? a[i] + dp[i + 1][j] : MAX), (j < n ? b[j] + dp[i][j + 1] : MAX));
+            }
+        }
+        return dp[0][0];
+    }
 
 
 }
