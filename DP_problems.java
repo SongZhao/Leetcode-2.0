7b86f542a69053a04f6fae51fc0d3a739218652f
SongZhao <lanmovii@gmail.com>
2018-06-01 23:21:37 -0500

    DP

--- DP_problems.java
+++ DP_problems.java
@@ -917,7 +917,40 @@ public class DP_problems{
         }
         return dp[0][0];
     }
-
-
+    //636 Maximum Length of Pair Chain
+    /*
+    /You are given n pairs of numbers. In every pair, the first number is always smaller than the second number.
+    /Now, we define a pair (c, d) can follow another pair (a, b) if and only if b < c. Chain of pairs can be formed in this fashion.
+    /Given a set of pairs, find the length longest chain which can be formed. You needn't use up all the given pairs. You can select pairs in any order.
+    /Input: [[1,2], [2,3], [3,4]]
+    /Output: 2
+    /Explanation: The longest chain is [1,2] -> [3,4]
+    /
+    */
+    //greedy solution 
+    public int findLongestChain(int[][] pairs) {
+        int re=0,form=Integer.MIN_VALUE;
+        Arrays.sort(pairs, (p1, p2) -> (p1[1] - p2[1]));
+        for(int i=0;i<pairs.length;i++){
+            if(pairs[i][0]>form){
+                form=pairs[i][1];
+                re++;
+            }
+        }
+        return re;
+    }
+    //DP
+    public int findLongestChain(int[][] pairs) {
+        if (pairs == null || pairs.length == 0) return 0;
+        Arrays.sort(pairs, (a, b) -> (a[0] - b[0]));
+        int[] dp = new int[pairs.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[i], pairs[i][0] > pairs[j][1]? dp[j] + 1 : dp[j]);
+            }
+        }
+        return dp[pairs.length - 1];
+    }
 }
 
